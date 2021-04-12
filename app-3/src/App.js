744487e5843ad590c712b11b0f730aa11032ca79
Brady Pitts <this.is.bp67c@gmail.com>
2021-04-12 16:50:07 -0600

    end of day push

--- app-3/src/App.js
+++ app-3/src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+      data:[
+        {
+          type: 'food',
+          name: 'apple'
+        },
+        {
+          type: 'food',
+          name: 'banana'
+        },
+        {
+          type: 'food',
+          name: 'peach'
+        },
+        {
+          type: 'utensil',
+          name: 'fork'
+        },
+        {
+          type: 'utensil',
+          name: 'spoon'
+        },
+        {
+          type: 'utensil',
+          name: 'knife'
+        }
+      ]
+    }
+  }
+
+
+  search(arr){
+    
+  }
+
+
+  render(){
+    return (
+      <div className="App">
+       
+      </div>
+    );
+  }
 }
 
 export default App;
